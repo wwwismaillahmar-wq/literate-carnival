da0633a74139929f52efb3128cfbec1291b70f71
Ismail Lahmar <www.ismaillahmar@gmail.com>
2026-09-09 13:08:24 +0100

    Add Home component with welcome message in Arabic

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
+      <h1>مرحباً بكم في ASLAN Group</h1>
+      <p>الموقع قيد التشغيل بنجاح!</p>
+    </div>
+  );
+}
