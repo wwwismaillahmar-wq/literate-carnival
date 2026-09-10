5a398d6d928c27da9263e53267f03121406de17a
Ismail Lahmar <www.ismaillahmar@gmail.com>
2026-09-10 01:42:18 +0100

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,8 @@
-import { defineConfig, globalIgnores } from 'eslint/config';
 import nextVitals from 'eslint-config-next/core-web-vitals';
-export default defineConfig([...nextVitals, globalIgnores(['.next/**','node_modules/**'])]);
+
+export default [
+  ...nextVitals,
+  {
+    ignores: ['.next/**', 'node_modules/**'],
+  },
+];
