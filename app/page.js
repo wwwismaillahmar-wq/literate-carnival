cf68eb16191e088500308f437c5195096a2f55c2
Ismail Lahmar <www.ismaillahmar@gmail.com>
2026-09-10 04:28:22 +0100

    Delete page.js

--- app/page.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', padding: '50px', fontFamily: 'sans-serif' }}>
-      <h1>مرحباً بكم في ASLAN Group</h1>
-      <p>الموقع قيد التشغيل بنجاح!</p>
-    </div>
-  );
-}
